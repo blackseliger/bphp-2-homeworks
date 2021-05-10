60b5c3dc14e2b84b5c9fd601d07a10c8551b9c23
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-05-10 19:56:16 +0300

    git commit -m "add 3 tasks"

---
+++ 001-intro/index.php
@@ -0,0 +1,48 @@
+<?php
+
+echo "Hello, world!";
+
+//  2.1 task
+const ANOTHER_CONST = 'Прощай, мир.';
+echo __FILE__;
+echo ANOTHER_CONST . __LINE__;
+
+// heredoc 2.2 task
+
+$strHeredoc = <<<EOD
+ Я студент Нетологии
+EOD;
+echo $strHeredoc;
+
+// 2.3 task
+
+$a = 'Fish';
+$b = 'human';
+
+echo <<<EOD
+ $a рыбою сыта, а $b человеком.
+EOD;
+
+// 3.1 task Desc type
+
+$variable = 3.14;
+
+if (is_bool($variable)) {
+    echo ' bool';
+} else if (is_float($variable)) {
+    echo ' float';
+} else if (is_int($variable)) {
+    echo ' int';
+} else if (is_string($variable)) {
+    echo ' string';
+} else if (is_null($variable)) {
+    echo ' null';
+} else if (is_array($variable) || is_object($variable) || is_resource($variable)) {
+    echo ' other';
+} else {
+    echo 'false';
+}
+
+
+
+?>
